04540f081e1fa442d437ffd736e170fd9a99d3ff
seoyh0812 <76783110+seoyh0812@users.noreply.github.com>
2021-07-14 18:14:57 +0900

    2021-07-14
    UI - 0.1ver

---
+++ among-us/Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse
+}
+
+public class PlayerSettings : MonoBehaviour
+{
+    public static EControlType controlType;
+}
---
+++ among-us/Assets/Scripts/SettingsUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+	[SerializeField]
+	private Button MouseControlButton;
+	[SerializeField]
+	private Button KeyboardMouseControlButton;
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+}
